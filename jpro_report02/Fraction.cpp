3842ed0da308066a66255b0b7456dcf7b47d1186
HarukaKajita <kajitaj63b3@gmail.com>
2019-05-18 03:35:11 +0900

    ファイル分割

---
+++ jpro_report02/Fraction.cpp
@@ -0,0 +1,129 @@
+#include "Fraction.h"
+//ƒRƒ“ƒXƒgƒ‰ƒNƒ^
+//default : numrator = 0, denominator = 1
+Fraction::Fraction(int numrator, int denominator) {
+	if (denominator == 0) {
+		cerr << "•ª•ê‚É0‚ªÝ’è‚³‚ê‚Ä‚¢‚Ü‚·" << endl;
+		exit(EXIT_FAILURE);
+	}
+	this->numrator = numrator;
+	this->denominator = denominator;
+	reduce();
+	adjustSign();
+}
+
+//Žl‘¥‰‰ŽZŽq
+void Fraction::operator+=(Fraction frac) {
+	//•ª•ê‚ÌÅ¬Œö”{”‚ð‹‚ß‚Ä•ª•ê‚ð‘µ‚¦‚Ä‰ÁŽZ
+	//¦c++17 or later
+	int LCM = lcm(this->getDenominator(), frac.getDenominator());
+	int mul0 = LCM / this->getDenominator();
+	int mul1 = LCM / frac.getDenominator();
+
+	int numrator = mul0 * this->getNumerator() + mul1 * frac.getNumerator();
+	int denominator = LCM;
+
+	this->numrator = numrator;
+	this->denominator = denominator;
+	reduce();
+	adjustSign();
+}
+void Fraction::operator-=(Fraction frac) {
+	//•ª•ê‚ÌÅ¬Œö”{”‚ð‹‚ß‚Ä•ª•ê‚ð‘µ‚¦‚ÄŒ¸ŽZ
+	//¦c++17 or later
+	int LCM = lcm(this->getDenominator(), frac.getDenominator());
+	int mul0 = LCM / this->getDenominator();
+	int mul1 = LCM / frac.getDenominator();
+
+	int numrator = mul0 * this->getNumerator() - mul1 * frac.getNumerator();
+	int denominator = LCM;
+
+	this->numrator = numrator;
+	this->denominator = denominator;
+	reduce();
+	adjustSign();
+}
+void Fraction::operator*=(Fraction frac) {
+	this->numrator *= frac.getNumerator(); this->denominator *= frac.getDenominator();
+	reduce();
+	adjustSign();
+}
+void Fraction::operator/=(Fraction frac) {
+	this->numrator *= frac.getDenominator(); this->denominator *= frac.getNumerator();
+	reduce();
+	adjustSign();
+}
+
+Fraction operator+(Fraction f0, Fraction f1) {
+	f0 += f1;
+	return f0;
+}
+Fraction operator-(Fraction f0, Fraction f1) {
+	f0 -= f1;
+	return f0;
+}
+Fraction operator*(Fraction f0, Fraction f1) {
+	f0 *= f1;
+	return f0;
+}
+Fraction operator/(Fraction f0, Fraction f1) {
+	f0 /= f1;
+	return f0;
+}
+
+//”äŠr‰‰ŽZŽq
+bool Fraction::operator<(const Fraction frac) const {
+	return this->getScaler() < frac.getScaler();
+}
+bool Fraction::operator>(const Fraction frac) const {
+	return this->getScaler() > frac.getScaler();
+}
+bool Fraction::operator==(const Fraction frac) const {
+	return this->getScaler() == frac.getScaler();
+}
+
+//“üo—Íi“ü—Í‚Ì‚Ýfriendj
+istream& operator>>(istream& stream, Fraction& frac) {
+	int numerator, denominator;
+	stream >> numerator >> denominator;
+	if (denominator == 0) {
+		cerr << "•ª•ê‚É0‚ªÝ’è‚³‚ê‚Ä‚¢‚Ü‚·" << endl;
+		exit(EXIT_FAILURE);
+	}
+	frac.numrator = numerator;
+	frac.denominator = denominator;
+	frac.reduce();
+	frac.adjustSign();
+	return stream;
+}
+ostream& operator<<(ostream& stream, const Fraction frac) {
+	if (frac.getDenominator() == 1 || frac.getNumerator() == 0) stream << frac.getNumerator();
+	else stream << "(" << frac.getNumerator() << "/" << frac.getDenominator() << ")";
+	return stream;
+}
+
+//–ñ•ª
+void Fraction::reduce() {
+	//¦c++17 or later
+	int GCD = gcd(this->getNumerator(), this->getDenominator());
+	if (GCD == 0) return;
+	this->numrator /= GCD;
+	this->denominator /= GCD;
+}
+
+bool Fraction::isMinus() const {
+	bool isMinus = signbit(getScaler()) != 0;
+	return isMinus;
+}
+
+//•„†®—
+void Fraction::adjustSign() {
+	if (this->isMinus()) {
+		this->denominator = abs(this->getDenominator());
+		this->numrator = abs(this->getNumerator()) * -1;
+	}
+	else {
+		this->denominator = abs(this->getDenominator());
+		this->numrator = abs(this->getNumerator());
+	}
+}
---
+++ jpro_report02/Fraction.h
@@ -0,0 +1,43 @@
+#pragma once
+#include <iostream>
+#include <cstdlib>
+#include <numeric> //for std::gcd() std::lcm()
+#include <cmath>   //for signbit()
+using namespace std;
+
+class Fraction {
+	//numrator    : 分子
+	//denominator : 分母
+private:
+	int numrator, denominator;
+	//符号整理
+	void adjustSign();
+	void reduce();
+
+public:
+	Fraction(int numerator = 0, int denominator = 1);
+	int getNumerator()   const { return numrator; }
+	int getDenominator() const { return denominator; }
+	//double型小数表現に変換した値を返す
+	double getScaler() const { return (double)numrator / denominator; }
+	bool isMinus() const;
+	//四則演算
+	void operator+=(Fraction frac);
+	void operator-=(Fraction frac);
+	void operator*=(Fraction frac);
+	void operator/=(Fraction frac);
+	bool operator<(const Fraction frac) const;
+	bool operator>(const Fraction frac) const;
+	bool operator==(const Fraction frac) const;
+	friend istream& operator>>(istream& stream, Fraction& frac);
+};
+
+//四則演算
+Fraction operator+(Fraction f0, Fraction f1);
+Fraction operator-(Fraction f0, Fraction f1);
+Fraction operator*(Fraction f0, Fraction f1);
+Fraction operator/(Fraction f0, Fraction f1);
+
+//入出力（入力のみfriend）
+istream& operator>>(istream& stream, Fraction& frac);
+ostream& operator<<(ostream& stream, const Fraction frac);
